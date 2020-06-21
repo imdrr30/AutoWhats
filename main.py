f7686b737033e9ddf8ecf50a839d0e0f9f2b62de
Revan <43107289+revanrohith@users.noreply.github.com>
2020-06-21 19:16:11 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
+import datetime
 
 chrome_options = webdriver.ChromeOptions()
 #chrome_options.add_argument("--headless")
@@ -10,7 +11,6 @@ chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:77.0) Gecko/20100101 Firefox/77.0")
 driver = webdriver.Chrome("/Users/revan/PycharmProjects/Whtsmon/Driver/chromedriver", chrome_options=chrome_options)
 driver.get("https://web.whatsapp.com")
-
 
 
 while True:
@@ -29,19 +29,32 @@ while True:
       print("Group Detected Unread")
       continue
     else:
-      user.click()
-      print(itext)
-      lastm = driver.find_elements_by_xpath('//span[@class = "_3Whw5 selectable-text invisible-space copyable-text"]')
-      tmp = lastm[-1].get_attribute('innerHTML')
-      s = tmp.find("</span>")
-      lmsg = tmp[6:s]
-      txtbx = driver.find_element_by_xpath('//div[@spellcheck = "true"]')
-      sbb = driver.find_elements_by_xpath('//div[@class="_1JNuk"]')
-      if lmsg=='busy ?':
-        txtbx.send_keys("Yep call you later")
-        txtbx.send_keys(Keys.RETURN)
+      print("User Detected Unread")
+      mtime = driver.find_element_by_xpath('//div[@class = "m61XR"]')
+      ctime = mtime.get_attribute('innerHTML')
+      current_date_and_time = datetime.datetime.now()
+      hours_added = datetime.timedelta(hours=5, minutes=30)
+      current_date_and_time = current_date_and_time + hours_added
+      if ctime[2]==':':
+        ctime= ctime[:5]
+      elif ctime[1]==':':
+        ctime = ctime[:4]
+      if current_date_and_time.strftime("%H:%M")>=ctime:
+        user.click()
+        print(itext)
+        lastm = driver.find_elements_by_xpath('//span[@class = "_3Whw5 selectable-text invisible-space copyable-text"]')
+        tmp = lastm[-1].get_attribute('innerHTML')
+        s = tmp.find("</span>")
+        lmsg = tmp[6:s]
+        txtbx = driver.find_element_by_xpath('//div[@spellcheck = "true"]')
+        sbb = driver.find_elements_by_xpath('//div[@class="_1JNuk"]')
+        if lmsg=='busy ?':
+          txtbx.send_keys("Yep call you later")
+          txtbx.send_keys(Keys.RETURN)
+        else:
+          txtbx.send_keys("The Person you reached is Offline and will text you soon")
+          txtbx.send_keys(Keys.RETURN)
       else:
-        txtbx.send_keys("The Person you reached is Offline and will text you soon")
-        txtbx.send_keys(Keys.RETURN)
+        continue
   except:
     pass
